fd4c88068af79993b8fd80ea8b698df17f9d3054
ksglobalservices <ksglobalservicesllc1@gmail.com>
2026-04-17 14:30:12 -0400

    integrate form jotform of the admin

--- app/(public)/formList/[adminId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function FormList() {
-  return (
-    <div>
-      <h1>Form List</h1>
-    </div>
-  );
-}
--- lib/form-category.ts
+++ lib/form-category.ts
@@ -1,22 +1,22 @@
 export const FORMS_MAP: Record<string, { title: string; jotformId: string }> = {
   "immigration-uscis-support": {
     title: "Immigration & USCIS Support",
-    jotformId: "261005378903859",
+    jotformId: "261065849491062",
   },
   "tax-financial-services": {
     title: "Tax & Financial Services",
-    jotformId: "252944587965881",
+    jotformId: "261066022233040",
   },
   "notary-document-services": {
     title: "Notary & Document Services",
-    jotformId: "261005339541854",
+    jotformId: "261065853442053",
   },
   "business-administrative-services": {
     title: "Business & Administrative Services",
-    jotformId: "261005740944857",
+    jotformId: "261065787623060",
   },
   "translation-language-services": {
     title: "Translation & Language Services",
-    jotformId: "261005704582855",
+    jotformId: "261065218531047",
   },
 };
